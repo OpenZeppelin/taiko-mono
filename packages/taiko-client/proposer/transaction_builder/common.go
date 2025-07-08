7d5d75f94e123cc4d597d51a1dc4175de37c18f5
Leo <leonard.paturel@openzeppelin.com>
2025-07-08 16:11:12 +0200

    remove calldata logic proposer

--- packages/taiko-client/proposer/transaction_builder/common.go
+++ packages/taiko-client/proposer/transaction_builder/common.go
@@ -2,14 +2,10 @@ package builder
 
 import (
 	"context"
-	"math/big"
 
 	"github.com/ethereum-optimism/optimism/op-service/txmgr"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-
-	"github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/encoding"
-	pacayaBindings "github.com/taikoxyz/taiko-mono/packages/taiko-client/bindings/pacaya"
 )
 
 // ProposeBlocksTransactionBuilder is an interface for building a TaikoInbox.proposeBatch
@@ -23,28 +19,4 @@ type ProposeBlocksTransactionBuilder interface {
 		// minTxsPerForcedInclusion *big.Int,
 		parentMetahash common.Hash,
 	) (*txmgr.TxCandidate, error)
-}
-
-// buildParamsForForcedInclusion builds the blob params and the block params
-// for the given forced inclusion.
-func buildParamsForForcedInclusion(
-	forcedInclusion *pacayaBindings.IForcedInclusionStoreForcedInclusion,
-	minTxsPerForcedInclusion *big.Int,
-) (*encoding.BlobParams, []pacayaBindings.ITaikoInboxBlockParams) {
-	if forcedInclusion == nil {
-		return nil, nil
-	}
-	return &encoding.BlobParams{
-			BlobHashes: [][32]byte{forcedInclusion.BlobHash},
-			NumBlobs:   0,
-			ByteOffset: forcedInclusion.BlobByteOffset,
-			ByteSize:   forcedInclusion.BlobByteSize,
-			CreatedIn:  forcedInclusion.BlobCreatedIn,
-		}, []pacayaBindings.ITaikoInboxBlockParams{
-			{
-				NumTransactions: uint16(minTxsPerForcedInclusion.Uint64()),
-				TimeShift:       0,
-				SignalSlots:     make([][32]byte, 0),
-			},
-		}
 }
